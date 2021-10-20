2b0a0c7d7be133f3d298affe48b7ae0ac970d84c
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-10-20 12:10:46 +0200

    done

---
+++ snack-3/index.php
@@ -0,0 +1,81 @@
+<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+<?php
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+
+        <ul>
+            <?php foreach($posts as $dates => $lists): ?>
+
+                <li>
+                    <h3><?php echo $dates; ?> </h3> 
+
+                    <ul>
+
+                        <?php foreach($lists as $element): ?>
+
+                            <li>
+                                <h2><?php echo $element['title']; ?> </h2>
+                                <h3><?php echo $element['author']; ?> </h3>
+                                <p><?php echo $element['text']; ?> </p>
+
+                            </li>
+
+                        <?php endforeach; ?>    
+
+                    </ul>
+                </li>
+
+            <?php endforeach; ?>    
+        </ul>
+        
+    </body>
+</html>
